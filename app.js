9eae57944e7919051df9e9a3107557ddbd780e9c
IVEWOR <howdy@deepslog.com>
2024-05-25 12:28:16 +0530

    db connection and all

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const dotenv = require("dotenv");
+
+// routes
+const authRoute = require("./routes/Auth");
+const postRoute = require("./routes/Posts");
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // DB connection
-Connect.CONNECTDB(process.env.URL);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+// middleware
+app.use(express.json());
+app.use("/api/blog", authRoute);
+app.use("/api/blog/posts", postRoute);
 
 // Port Listener
 app.listen(port, () => console.log(`Listening on port: ${port}`));
